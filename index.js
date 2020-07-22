6946186e24d14c77ed6102b20868fc953caef95a
denis.savgir@codeimmersives.com <denis.savgir@codeimmersives.com>
2020-07-22 10:13:13 -0400

    Initial setup for non-AI features

--- index.js
+++ index.js
@@ -1,15 +1,77 @@
-'use strict';
-require('dotenv').config();
 const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+const path = require('path');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const { check, validationResult } = require('express-validator');
+const { userInfo } = require('os');
+let MongoStore = require('connect-mongo')(session)
+const bcrypt = require('bcryptjs');
+const { nanoid } = require("nanoid");
+//const { x } = require("./myModule");
+const User = require('./models/User');
+require('./lib/passport');
+const mailjet = require ('node-mailjet')
+.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
+
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cookieParser('process.env.SECRET'));
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.SESSION_SECRET,
+	store: new MongoStore({
+		url: process.env.MONGODB_URI,
+		mongooseConnection: mongoose.connection,
+		autoReconnect: true
+	}),
+	cookie: {
+		secure: false,
+		maxAge: 1000 * 60 * 5
+	}
+}))
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+	console.log('Session: ', req.session);
+	console.log('User: ', req.user);
+	next();
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err=> console.log('MongoDB Error: ', err))
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // html
+app.use(express.static(path.join(__dirname, 'public'))); // js, css, images
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// Main bot stuff below
 const aibot = require('./aibot'); // Loads AI bot
 const cors = require('cors');
 const getWeather = require('./getWeather'); // Weather functionality
 const misc = require('./misc'); // Misc functionality
 
-const app = express();
-app.use(cors());
-app.use(express.static(__dirname + '/views')); // html
-app.use(express.static(__dirname + '/public')); // js, css, images
+//const app = express();
 
 // Set up express web server
 const server = app.listen(process.env.PORT || 5000, () => {
@@ -23,9 +85,9 @@ io.on('connection', function(socket){
 });
 
 // Web UI
-app.get('/', (req, res) => {
-	res.sendFile('index.html');
-});
+// app.get('/', (req, res) => {
+// 	res.sendFile('index.html');
+// });
 
 io.on('connection', function(socket) {
 	socket.on('chat message', (text) => {
